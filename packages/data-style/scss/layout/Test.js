fa147057f6347b1ca8d9860711b0b32dcd6b23da
郑翔 <uni-zheng@qq.com>
2020-08-10 11:42:17 +0800

    feat: 新增 data-style package

---
+++ packages/data-style/scss/layout/Test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Test.propTypes = {
+
+};
+
+Test.displayName = 'Test';
+
+function Test(props) {
+  return (
+    <div>123</div>
+  );
+}
+
+export default Test;
